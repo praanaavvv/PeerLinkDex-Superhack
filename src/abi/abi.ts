973eaef643e0d4a90336b6a7cec8f49275886434
praanaavvv <dev.agarwal428@gmail.com>
2024-08-10 20:08:07 +0530

    updated Trade page

---
+++ src/abi/abi.ts
@@ -0,0 +1,584 @@
+export const EscrowABI = [
+    {
+      "type": "function",
+      "name": "cancelEscrowArrangement",
+      "inputs": [
+        { "name": "_escrowId", "type": "uint256", "internalType": "uint256" }
+      ],
+      "outputs": [],
+      "stateMutability": "payable"
+    },
+    {
+      "type": "function",
+      "name": "completeEscrowArrangement",
+      "inputs": [
+        { "name": "_escrowId", "type": "uint256", "internalType": "uint256" }
+      ],
+      "outputs": [],
+      "stateMutability": "payable"
+    },
+    {
+      "type": "function",
+      "name": "createEscrowArrangement",
+      "inputs": [
+        { "name": "participant", "type": "address", "internalType": "address" },
+        {
+          "name": "tokenInAddress",
+          "type": "address",
+          "internalType": "address"
+        },
+        {
+          "name": "tokenOutAddress",
+          "type": "address",
+          "internalType": "address"
+        },
+        { "name": "amountIn", "type": "uint256", "internalType": "uint256" },
+        { "name": "amountOut", "type": "uint256", "internalType": "uint256" },
+        { "name": "duration", "type": "uint256", "internalType": "uint256" }
+      ],
+      "outputs": [],
+      "stateMutability": "payable"
+    },
+    {
+      "type": "function",
+      "name": "escrowId",
+      "inputs": [],
+      "outputs": [{ "name": "", "type": "uint256", "internalType": "uint256" }],
+      "stateMutability": "view"
+    },
+    {
+      "type": "function",
+      "name": "escrowInfo",
+      "inputs": [{ "name": "", "type": "uint256", "internalType": "uint256" }],
+      "outputs": [
+        { "name": "escrowId", "type": "uint256", "internalType": "uint256" },
+        { "name": "initiator", "type": "address", "internalType": "address" },
+        { "name": "participant", "type": "address", "internalType": "address" },
+        {
+          "name": "tokenInAddress",
+          "type": "address",
+          "internalType": "address"
+        },
+        {
+          "name": "tokenOutAddress",
+          "type": "address",
+          "internalType": "address"
+        },
+        { "name": "amountIn", "type": "uint256", "internalType": "uint256" },
+        { "name": "amountOut", "type": "uint256", "internalType": "uint256" },
+        {
+          "name": "startingTimestamp",
+          "type": "uint256",
+          "internalType": "uint256"
+        },
+        { "name": "duration", "type": "uint256", "internalType": "uint256" },
+        {
+          "name": "state",
+          "type": "uint8",
+          "internalType": "enum Escrow.EscrowState"
+        }
+      ],
+      "stateMutability": "view"
+    },
+    {
+      "type": "function",
+      "name": "getEscrowId",
+      "inputs": [],
+      "outputs": [{ "name": "", "type": "uint256", "internalType": "uint256" }],
+      "stateMutability": "view"
+    },
+    {
+      "type": "function",
+      "name": "getEscrowInfo",
+      "inputs": [
+        { "name": "_escrowId", "type": "uint256", "internalType": "uint256" }
+      ],
+      "outputs": [
+        {
+          "name": "",
+          "type": "tuple",
+          "internalType": "struct Escrow.EscrowDetails",
+          "components": [
+            {
+              "name": "escrowId",
+              "type": "uint256",
+              "internalType": "uint256"
+            },
+            {
+              "name": "initiator",
+              "type": "address",
+              "internalType": "address"
+            },
+            {
+              "name": "participant",
+              "type": "address",
+              "internalType": "address"
+            },
+            {
+              "name": "tokenInAddress",
+              "type": "address",
+              "internalType": "address"
+            },
+            {
+              "name": "tokenOutAddress",
+              "type": "address",
+              "internalType": "address"
+            },
+            {
+              "name": "amountIn",
+              "type": "uint256",
+              "internalType": "uint256"
+            },
+            {
+              "name": "amountOut",
+              "type": "uint256",
+              "internalType": "uint256"
+            },
+            {
+              "name": "startingTimestamp",
+              "type": "uint256",
+              "internalType": "uint256"
+            },
+            {
+              "name": "duration",
+              "type": "uint256",
+              "internalType": "uint256"
+            },
+            {
+              "name": "state",
+              "type": "uint8",
+              "internalType": "enum Escrow.EscrowState"
+            }
+          ]
+        }
+      ],
+      "stateMutability": "view"
+    },
+    {
+      "type": "event",
+      "name": "EscrowEvent",
+      "inputs": [
+        {
+          "name": "escrowId",
+          "type": "uint256",
+          "indexed": true,
+          "internalType": "uint256"
+        },
+        {
+          "name": "escrowDetails",
+          "type": "tuple",
+          "indexed": false,
+          "internalType": "struct Escrow.EscrowDetails",
+          "components": [
+            {
+              "name": "escrowId",
+              "type": "uint256",
+              "internalType": "uint256"
+            },
+            {
+              "name": "initiator",
+              "type": "address",
+              "internalType": "address"
+            },
+            {
+              "name": "participant",
+              "type": "address",
+              "internalType": "address"
+            },
+            {
+              "name": "tokenInAddress",
+              "type": "address",
+              "internalType": "address"
+            },
+            {
+              "name": "tokenOutAddress",
+              "type": "address",
+              "internalType": "address"
+            },
+            {
+              "name": "amountIn",
+              "type": "uint256",
+              "internalType": "uint256"
+            },
+            {
+              "name": "amountOut",
+              "type": "uint256",
+              "internalType": "uint256"
+            },
+            {
+              "name": "startingTimestamp",
+              "type": "uint256",
+              "internalType": "uint256"
+            },
+            {
+              "name": "duration",
+              "type": "uint256",
+              "internalType": "uint256"
+            },
+            {
+              "name": "state",
+              "type": "uint8",
+              "internalType": "enum Escrow.EscrowState"
+            }
+          ]
+        }
+      ],
+      "anonymous": false
+    }
+  ] as const;
+
+export const TokenInABI = [
+  { "type": "constructor", "inputs": [], "stateMutability": "nonpayable" },
+  {
+    "type": "function",
+    "name": "allowance",
+    "inputs": [
+      { "name": "owner", "type": "address", "internalType": "address" },
+      { "name": "spender", "type": "address", "internalType": "address" }
+    ],
+    "outputs": [{ "name": "", "type": "uint256", "internalType": "uint256" }],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "approve",
+    "inputs": [
+      { "name": "spender", "type": "address", "internalType": "address" },
+      { "name": "value", "type": "uint256", "internalType": "uint256" }
+    ],
+    "outputs": [{ "name": "", "type": "bool", "internalType": "bool" }],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "balanceOf",
+    "inputs": [
+      { "name": "account", "type": "address", "internalType": "address" }
+    ],
+    "outputs": [{ "name": "", "type": "uint256", "internalType": "uint256" }],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "decimals",
+    "inputs": [],
+    "outputs": [{ "name": "", "type": "uint8", "internalType": "uint8" }],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "name",
+    "inputs": [],
+    "outputs": [{ "name": "", "type": "string", "internalType": "string" }],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "symbol",
+    "inputs": [],
+    "outputs": [{ "name": "", "type": "string", "internalType": "string" }],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "totalSupply",
+    "inputs": [],
+    "outputs": [{ "name": "", "type": "uint256", "internalType": "uint256" }],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "transfer",
+    "inputs": [
+      { "name": "to", "type": "address", "internalType": "address" },
+      { "name": "value", "type": "uint256", "internalType": "uint256" }
+    ],
+    "outputs": [{ "name": "", "type": "bool", "internalType": "bool" }],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "transferFrom",
+    "inputs": [
+      { "name": "from", "type": "address", "internalType": "address" },
+      { "name": "to", "type": "address", "internalType": "address" },
+      { "name": "value", "type": "uint256", "internalType": "uint256" }
+    ],
+    "outputs": [{ "name": "", "type": "bool", "internalType": "bool" }],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "event",
+    "name": "Approval",
+    "inputs": [
+      {
+        "name": "owner",
+        "type": "address",
+        "indexed": true,
+        "internalType": "address"
+      },
+      {
+        "name": "spender",
+        "type": "address",
+        "indexed": true,
+        "internalType": "address"
+      },
+      {
+        "name": "value",
+        "type": "uint256",
+        "indexed": false,
+        "internalType": "uint256"
+      }
+    ],
+    "anonymous": false
+  },
+  {
+    "type": "event",
+    "name": "Transfer",
+    "inputs": [
+      {
+        "name": "from",
+        "type": "address",
+        "indexed": true,
+        "internalType": "address"
+      },
+      {
+        "name": "to",
+        "type": "address",
+        "indexed": true,
+        "internalType": "address"
+      },
+      {
+        "name": "value",
+        "type": "uint256",
+        "indexed": false,
+        "internalType": "uint256"
+      }
+    ],
+    "anonymous": false
+  },
+  {
+    "type": "error",
+    "name": "ERC20InsufficientAllowance",
+    "inputs": [
+      { "name": "spender", "type": "address", "internalType": "address" },
+      { "name": "allowance", "type": "uint256", "internalType": "uint256" },
+      { "name": "needed", "type": "uint256", "internalType": "uint256" }
+    ]
+  },
+  {
+    "type": "error",
+    "name": "ERC20InsufficientBalance",
+    "inputs": [
+      { "name": "sender", "type": "address", "internalType": "address" },
+      { "name": "balance", "type": "uint256", "internalType": "uint256" },
+      { "name": "needed", "type": "uint256", "internalType": "uint256" }
+    ]
+  },
+  {
+    "type": "error",
+    "name": "ERC20InvalidApprover",
+    "inputs": [
+      { "name": "approver", "type": "address", "internalType": "address" }
+    ]
+  },
+  {
+    "type": "error",
+    "name": "ERC20InvalidReceiver",
+    "inputs": [
+      { "name": "receiver", "type": "address", "internalType": "address" }
+    ]
+  },
+  {
+    "type": "error",
+    "name": "ERC20InvalidSender",
+    "inputs": [
+      { "name": "sender", "type": "address", "internalType": "address" }
+    ]
+  },
+  {
+    "type": "error",
+    "name": "ERC20InvalidSpender",
+    "inputs": [
+      { "name": "spender", "type": "address", "internalType": "address" }
+    ]
+  }
+] as const;
+
+export const TokenOutABI = [
+  { "type": "constructor", "inputs": [], "stateMutability": "nonpayable" },
+  {
+    "type": "function",
+    "name": "allowance",
+    "inputs": [
+      { "name": "owner", "type": "address", "internalType": "address" },
+      { "name": "spender", "type": "address", "internalType": "address" }
+    ],
+    "outputs": [{ "name": "", "type": "uint256", "internalType": "uint256" }],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "approve",
+    "inputs": [
+      { "name": "spender", "type": "address", "internalType": "address" },
+      { "name": "value", "type": "uint256", "internalType": "uint256" }
+    ],
+    "outputs": [{ "name": "", "type": "bool", "internalType": "bool" }],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "balanceOf",
+    "inputs": [
+      { "name": "account", "type": "address", "internalType": "address" }
+    ],
+    "outputs": [{ "name": "", "type": "uint256", "internalType": "uint256" }],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "decimals",
+    "inputs": [],
+    "outputs": [{ "name": "", "type": "uint8", "internalType": "uint8" }],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "name",
+    "inputs": [],
+    "outputs": [{ "name": "", "type": "string", "internalType": "string" }],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "symbol",
+    "inputs": [],
+    "outputs": [{ "name": "", "type": "string", "internalType": "string" }],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "totalSupply",
+    "inputs": [],
+    "outputs": [{ "name": "", "type": "uint256", "internalType": "uint256" }],
+    "stateMutability": "view"
+  },
+  {
+    "type": "function",
+    "name": "transfer",
+    "inputs": [
+      { "name": "to", "type": "address", "internalType": "address" },
+      { "name": "value", "type": "uint256", "internalType": "uint256" }
+    ],
+    "outputs": [{ "name": "", "type": "bool", "internalType": "bool" }],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "function",
+    "name": "transferFrom",
+    "inputs": [
+      { "name": "from", "type": "address", "internalType": "address" },
+      { "name": "to", "type": "address", "internalType": "address" },
+      { "name": "value", "type": "uint256", "internalType": "uint256" }
+    ],
+    "outputs": [{ "name": "", "type": "bool", "internalType": "bool" }],
+    "stateMutability": "nonpayable"
+  },
+  {
+    "type": "event",
+    "name": "Approval",
+    "inputs": [
+      {
+        "name": "owner",
+        "type": "address",
+        "indexed": true,
+        "internalType": "address"
+      },
+      {
+        "name": "spender",
+        "type": "address",
+        "indexed": true,
+        "internalType": "address"
+      },
+      {
+        "name": "value",
+        "type": "uint256",
+        "indexed": false,
+        "internalType": "uint256"
+      }
+    ],
+    "anonymous": false
+  },
+  {
+    "type": "event",
+    "name": "Transfer",
+    "inputs": [
+      {
+        "name": "from",
+        "type": "address",
+        "indexed": true,
+        "internalType": "address"
+      },
+      {
+        "name": "to",
+        "type": "address",
+        "indexed": true,
+        "internalType": "address"
+      },
+      {
+        "name": "value",
+        "type": "uint256",
+        "indexed": false,
+        "internalType": "uint256"
+      }
+    ],
+    "anonymous": false
+  },
+  {
+    "type": "error",
+    "name": "ERC20InsufficientAllowance",
+    "inputs": [
+      { "name": "spender", "type": "address", "internalType": "address" },
+      { "name": "allowance", "type": "uint256", "internalType": "uint256" },
+      { "name": "needed", "type": "uint256", "internalType": "uint256" }
+    ]
+  },
+  {
+    "type": "error",
+    "name": "ERC20InsufficientBalance",
+    "inputs": [
+      { "name": "sender", "type": "address", "internalType": "address" },
+      { "name": "balance", "type": "uint256", "internalType": "uint256" },
+      { "name": "needed", "type": "uint256", "internalType": "uint256" }
+    ]
+  },
+  {
+    "type": "error",
+    "name": "ERC20InvalidApprover",
+    "inputs": [
+      { "name": "approver", "type": "address", "internalType": "address" }
+    ]
+  },
+  {
+    "type": "error",
+    "name": "ERC20InvalidReceiver",
+    "inputs": [
+      { "name": "receiver", "type": "address", "internalType": "address" }
+    ]
+  },
+  {
+    "type": "error",
+    "name": "ERC20InvalidSender",
+    "inputs": [
+      { "name": "sender", "type": "address", "internalType": "address" }
+    ]
+  },
+  {
+    "type": "error",
+    "name": "ERC20InvalidSpender",
+    "inputs": [
+      { "name": "spender", "type": "address", "internalType": "address" }
+    ]
+  }
+] as const;
--- src/components/Trade.tsx
+++ src/components/Trade.tsx
@@ -1,15 +1,238 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { useAccount, useWalletClient } from "wagmi";
-import addresses from "../utils/addresses";
-import { createWalletClient, custom } from "viem";
+import { createWalletClient, custom, Address } from "viem";
 import { baseSepolia } from "viem/chains";
+import addresses from "../utils/addresses";
+import "../styles/Trade.css"; // Import CSS file
+import baseSepoliaClient from "../utils/client";
+import { EscrowABI, TokenInABI, TokenOutABI } from "../abi/abi";
 
 const Trade: React.FC = () => {
-  const escrowContractAddress = addresses.escrow;
+  const { address, isConnected } = useAccount();
+  const { data: walletClient } = useWalletClient();
+  const escrowContractAddress: Address = addresses["escrow"] as Address;
+
+  const [escrowId, setEscrowId] = useState<number | undefined>(undefined);
+  const [participantAddress, setParticipantAddress] = useState<string>("");
+  const [tokenInAddress, setTokenInAddress] = useState<string>("");
+  const [tokenOutAddress, setTokenOutAddress] = useState<string>("");
+  const [amountIn, setAmountIn] = useState<number | undefined>(undefined);
+  const [amountOut, setAmountOut] = useState<number | undefined>(undefined);
+  const [duration, setDuration] = useState<number | undefined>(undefined);
+
+  // const createEscrow = async () => {
+  //   if (!isConnected || !walletClient || !address) return;
+
+  //   try {
+  //     const wallet = createWalletClient({
+  //       chain: baseSepolia,
+  //       transport: custom(walletClient),
+  //     });
+
+  //     const transaction = await wallet.writeContract({
+  //       address: escrowContractAddress,
+  //       account: address,
+  //       abi: EscrowABI,
+  //       functionName: "createEscrowArrangement",
+  //       args: [
+  //         participantAddress as Address,
+  //         tokenInAddress as Address,
+  //         tokenOutAddress as Address,
+  //         BigInt(amountIn as number),
+  //         BigInt(amountOut as number),
+  //         BigInt(duration as number),
+  //       ],
+  //     });
+
+  //     console.log("Transaction submitted:", transaction);
+  //   } catch (error) {
+  //     console.error("Failed to create escrow:", error);
+  //   }
+  // };
+  const createEscrow = async () => {
+    if (!isConnected || !walletClient || !address) return;
+
+    try {
+      const wallet = createWalletClient({
+        chain: baseSepolia,
+        transport: custom(walletClient),
+      });
+
+      // Check allowance for tokenIn
+      const allowanceResponse = await baseSepoliaClient.readContract({
+        address: tokenInAddress as Address,
+        abi: TokenInABI,
+        functionName: "allowance",
+        args: [address, escrowContractAddress],
+      });
+      const allowance = BigInt(allowanceResponse.toString());
+      const amountInBigNumber = BigInt(amountIn as number);
+
+      if (amountInBigNumber > allowance) {
+        // Request approval for the required amount
+        const approvalResponse = await wallet.writeContract({
+          address: tokenInAddress as Address,
+          account: address,
+          abi: TokenInABI,
+          functionName: "approve",
+          args: [escrowContractAddress, amountInBigNumber],
+        });
+
+        console.log("Token approved:", approvalResponse);
+        alert(
+          "Token approval is required. Please wait for the approval transaction to complete."
+        );
+        return; // Exit function until approval is complete
+      }
+
+      // Proceed with creating escrow arrangement
+      const transaction = await wallet.writeContract({
+        address: escrowContractAddress,
+        account: address,
+        abi: EscrowABI,
+        functionName: "createEscrowArrangement",
+        args: [
+          participantAddress as Address,
+          tokenInAddress as Address,
+          tokenOutAddress as Address,
+          amountInBigNumber,
+          BigInt(amountOut as number),
+          BigInt(duration as number),
+        ],
+      });
+
+      console.log("Transaction submitted:", transaction);
+    } catch (error) {
+      console.error("Failed to create escrow:", error);
+    }
+  };
+
+  const cancelEscrow = async () => {
+    if (!isConnected || !walletClient || !address) return;
+
+    try {
+      const wallet = createWalletClient({
+        chain: baseSepolia,
+        transport: custom(walletClient),
+      });
+
+      const transaction = await wallet.writeContract({
+        address: escrowContractAddress,
+        account: address,
+        abi: EscrowABI,
+        functionName: "cancelEscrowArrangement",
+        args: [BigInt(escrowId as number)],
+      });
+
+      console.log("Escrow cancelled:", transaction);
+    } catch (error) {
+      console.error("Failed to cancel escrow:", error);
+    }
+  };
+
+  const completeEscrow = async () => {
+    if (!isConnected || !walletClient || !address) return;
+
+    try {
+      const wallet = createWalletClient({
+        chain: baseSepolia,
+        transport: custom(walletClient),
+      });
+
+      const transaction = await wallet.writeContract({
+        address: escrowContractAddress,
+        account: address,
+        abi: EscrowABI,
+        functionName: "completeEscrowArrangement",
+        args: [BigInt(escrowId as number)],
+      });
+
+      console.log("Escrow completed:", transaction);
+    } catch (error) {
+      console.error("Failed to complete escrow:", error);
+    }
+  };
 
   return (
-    <div>
-      <p>Trade Here</p>
+    <div className="trade-container">
+      <h2>Trade Here</h2>
+      {isConnected ? (
+        <>
+          <p>Connected as: {address}</p>
+          <div className="form-container">
+            <div className="form-group">
+              <h3>Create Escrow</h3>
+              <input
+                type="text"
+                placeholder="Participant Address (e.g., 0x123...)"
+                value={participantAddress}
+                onChange={(e) => setParticipantAddress(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="Token In Address (e.g., 0xabc...)"
+                value={tokenInAddress}
+                onChange={(e) => setTokenInAddress(e.target.value)}
+              />
+              <input
+                type="text"
+                placeholder="Token Out Address (e.g., 0xdef...)"
+                value={tokenOutAddress}
+                onChange={(e) => setTokenOutAddress(e.target.value)}
+              />
+              <input
+                type="number"
+                placeholder="Amount In (e.g., 1000)"
+                value={amountIn}
+                onChange={(e) => setAmountIn(Number(e.target.value))}
+              />
+              <input
+                type="number"
+                placeholder="Amount Out (e.g., 2000)"
+                value={amountOut}
+                onChange={(e) => setAmountOut(Number(e.target.value))}
+              />
+              <input
+                type="number"
+                placeholder="Duration in Seconds (e.g., 86400)"
+                value={duration}
+                onChange={(e) => setDuration(Number(e.target.value))}
+              />
+              <button className="btn" onClick={createEscrow}>
+                Create Escrow
+              </button>
+            </div>
+
+            <div className="form-group">
+              <h3>Cancel Escrow</h3>
+              <input
+                type="number"
+                placeholder="Escrow ID"
+                value={escrowId}
+                onChange={(e) => setEscrowId(Number(e.target.value))}
+              />
+              <button className="btn" onClick={cancelEscrow}>
+                Cancel Escrow
+              </button>
+            </div>
+
+            <div className="form-group">
+              <h3>Complete Escrow</h3>
+              <input
+                type="number"
+                placeholder="Escrow ID"
+                value={escrowId}
+                onChange={(e) => setEscrowId(Number(e.target.value))}
+              />
+              <button className="btn" onClick={completeEscrow}>
+                Complete Escrow
+              </button>
+            </div>
+          </div>
+        </>
+      ) : (
+        <p>Please connect your wallet to start trading.</p>
+      )}
     </div>
   );
 };
